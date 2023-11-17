70c27155ab385587d26a3b1fbb3e7ee5f8c8d3e9
Sergey <144024686+alekshiga@users.noreply.github.com>
2023-11-17 19:02:57 +0700

    almost ready LR4

---
+++ inheritance/library/library/House.h
@@ -0,0 +1,119 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <iostream>
+
+class House {
+	protected:
+		std::string adress;
+		std::string phoneNumber;
+		int howManyKidsLiving;
+
+		friend std::string toString(const House house) {
+			return "Adress: " + house.adress + "Phone number: " + house.phoneNumber+ "Kids living: " + std::to_string(house.howManyKidsLiving) + "\n--------------------------------------\n";
+		}
+
+	public:
+		House() = default;
+
+		House(std::string adress, std::string phoneNumber, int howManyKidsLiving) {
+			this->adress = adress;
+			this->phoneNumber = phoneNumber;
+			this->howManyKidsLiving = howManyKidsLiving;
+		}
+
+		std::string getPhoneNumber() const {
+			return phoneNumber;
+		}
+
+		std::string getAdress() const {
+			return adress;
+		}
+
+		int getKidsInfo() const {
+			return howManyKidsLiving;
+		}
+
+		void setPhoneNumber(std::string phoneNumber) {
+			this->phoneNumber = phoneNumber;
+		}
+
+		void setKidsAmount(int howManyKidsLiving) {
+			this->howManyKidsLiving = howManyKidsLiving;
+		}
+
+		void operator++() {
+			this->howManyKidsLiving++;
+		}
+};
+
+House* createHouse() {
+	std::string adress;
+	std::cout << "Enter adress: " << std::endl;
+	std::cin >> adress;
+	std::string phoneNumber;
+	std::cout << "Enter phone number: " << std::endl;
+	std::cin >> phoneNumber;
+	int howManyKidsLiving;
+	std::cout << "How many kids living: " << std::endl;
+	std::cin >> howManyKidsLiving;
+	return new House(adress, phoneNumber, howManyKidsLiving);
+}
+
+class Flat : public House {
+
+private:
+	int flatNumber;
+	bool areNeighborsAngry;
+
+public:
+	Flat(std::string adress, int flatNumber, std::string phoneNumber, int howManyKidsLiving, bool areNeighborsAngry) {
+		this->adress = adress;
+		this->flatNumber = flatNumber;
+		this->phoneNumber = phoneNumber;
+		this->howManyKidsLiving = howManyKidsLiving;
+		this->areNeighborsAngry = areNeighborsAngry;
+	}
+
+	std::string getAdress() const {
+		std::string flatNumberString = std::to_string(flatNumber);
+		return adress + ", flat " + flatNumberString;
+	}
+
+	std::string toString() const {
+		std::string result;
+		result = "Adress: " + adress + ", flat ¹ " + std::to_string(flatNumber) + "\n" +
+		"Phone number: " + phoneNumber + "\n" + 
+		"Kids living: " + std::to_string(howManyKidsLiving) + "\n";
+		if (areNeighborsAngry) {
+			result += "Angry neighbors: yes\n";
+		}
+		else if (!areNeighborsAngry) {
+			result += "Angry neighbors: no\n";
+		}
+		else {
+			throw std::runtime_error("Should not reach here, invalid data");
+		}
+		result += "--------------------------------------\n";
+		return result;
+	}
+};
+
+House* createFlat() {
+	std::string adress;
+	std::cout << "Enter adress: " << std::endl;
+	std::cin >> adress;
+	int flatNumber;
+	std::cout << "Enter flat number: " << std::endl;
+	std::cin >> flatNumber;
+	std::string phoneNumber;
+	std::cout << "Enter phone number: " << std::endl;
+	std::cin >> phoneNumber;
+	int howManyKidsLiving;
+	std::cout << "How many kids living: " << std::endl;
+	std::cin >> howManyKidsLiving;
+	bool areNeighborsAngry;
+	std::cout << "If neighbors are angry enter 1, else enter 0: " << std::endl;
+	std::cin >> areNeighborsAngry;
+	return new Flat(adress, flatNumber, phoneNumber, howManyKidsLiving, areNeighborsAngry);
+}
---
+++ inheritance/library/library/library.cpp
@@ -0,0 +1,77 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <windows.h>
+#include <algorithm>
+#include <conio.h>
+#include "Animal.h"
+#include "Owner.h"
+#include "House.h"
+#include "Pet.h"
+#include "Residence.h"
+
+using namespace std;
+
+static int petQuantity;
+
+static std::vector<std::vector<std::string>> table;
+
+void createTable(int petQuantity, std::vector<Pet> pets) {
+    for (int i = 0; i < petQuantity; i++) {
+        Pet pet = pets[i];
+        table[i].push_back(pet.getAnimal()->getName());
+        table[i].push_back(pet.getAnimal()->getKind());
+        table[i].push_back(std::to_string(pet.getAnimal()->getAge()));
+        table[i].push_back(pet.getAnimal()->getSex());
+    }
+}
+
+void showTable() {
+    for (int i = 0; i < petQuantity; i++) {
+        for (int j = 0; j < 4; j++) {
+            std::cout << table[i][j] << "\t";
+        }
+        std::cout << std::endl;
+    }
+}
+
+Animal* vasya = new Cat("Vasya", "m", 5);
+Owner artem = Owner("Silyanov S.E.", "08.08.2004", 2);
+House* artemHouse = new House("Myatnaya 48", "+79831778095", 0);
+
+static std::vector<Owner> owners;
+static std::vector<Animal> animals;
+static std::vector<House> houses;
+static std::vector<Residence> residences;
+static std::vector<Pet> pets;
+
+static void petsToString() {
+    for (auto pet : pets) {
+        std::cout << std::endl;
+        std::cout << pet.toString();
+    }
+}
+
+int main() {
+    Animal* bobik = createDog();
+    Owner anton = createOwner();
+    House* antonFlat = createFlat();
+
+    animals.push_back(*vasya);
+    animals.push_back(*bobik);
+    owners.push_back(anton);
+    owners.push_back(artem);
+    houses.push_back(*antonFlat);
+    houses.push_back(*artemHouse);
+
+    Residence artemResidence = Residence(artemHouse, artem);
+    Residence antonResidence = Residence(antonFlat, anton);
+
+    Pet vasyaPet = Pet(vasya, artemResidence);
+    petQuantity++;
+    Pet bobikPet = Pet(bobik, antonResidence);
+    petQuantity++;
+
+    createTable(petQuantity, pets);
+    showTable();
+
+}
